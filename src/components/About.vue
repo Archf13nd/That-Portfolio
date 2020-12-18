a65650115fd29b59deb738170ff1fd5f5974eb3f
unknown <crazeddemonanarch@gmail.com>
2020-12-18 13:04:56 +0000

    finalized about page layout. Needs small adjustments still but the layout works as intended

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,4 +1,5 @@
 <template>
+<div class="screen-container">
   <div class="the-container">
     <div class="text--intro-container">
       <div class="text text--intro">
@@ -7,6 +8,7 @@
         with, is extremely satisfying to me.
       </div>
     </div>
+    <div class='spacer'></div>
 
     <img
       class="my-photo shadow-soft"
@@ -28,9 +30,83 @@
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <style lang="scss" scoped>
+
+.screen-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 0 auto;
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
 .the-container {
+  width: 60%;
+  height: 60%;
+  margin: 5% 0 5% 0;
+  justify-content: center;
+
+  display: grid;
+  grid-template: minmax(10px, 1fr) / repeat(auto-fill, minmax(300px, 600px));
+}
+
+.spacer {
+  display: inline-block;
+  background: rgba(0, 0, 0, 0);
+  width: 100%;
+  grid-column: 2 / 3;
+  grid-row: 1 / 2;
+
+  @media screen and (max-width: 1333px) {
+    display: none;
+  }
+}
+
+.my-photo {
+  height: 400px;
+  justify-self: end;
+  margin: 10% 10% 0 0;
+
+  @media screen and (max-width: 1333px) {
+  justify-self: center;
+  margin: 10% 0 10% 0;
+  } 
+  
+}
+
+.text {
+  color: #55de7b;
+  font-size: 18px;
+
+&--intro-container {
+  display: flex;
+  padding-right: 10%;
+  padding-left: 10%;
+  text-align: end;
+
+  @media screen and (max-width: 1333px) {
+  text-align: center;
+  } 
+}
+
+&__main--container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+&__main--2 {
+  margin-top: 5%;
+}
+
 }
 </style>
