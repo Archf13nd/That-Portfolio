913d8f6d6e3983be8c003683d86220861168d9b3
unknown <crazeddemonanarch@gmail.com>
2021-02-25 20:52:24 +0000

    finished website skill card

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -13,28 +13,28 @@
     <div class="skill-box bg-primary shadow-soft">
         <h2 class="skill-box__header">Optimized</h2>
         <div class="skill-box__content">
-          <div class="img"></div>
+          <div class="img fast"></div>
           <p>Even with lots of images or files, users will receive content asap!</p>
         </div>
       </div>
       <div class="skill-box bg-primary shadow-soft">
         <h2 class="skill-box__header">Pixel Perfect</h2>
         <div class="skill-box__content">
-          <div class="img"></div>
+          <div class="img cube"></div>
           <p class="text">If you provide the design I will treat in with respect. I uphold myself to pixel perfect standards.</p>
         </div>
       </div>
       <div class="skill-box bg-primary shadow-soft">
         <h2 class="skill-box__header">Communication</h2>
         <div class="skill-box__content">
-          <div class="img"></div>
+          <div class="img comms"></div>
           <p>I will provide you with periodic updates and together we can steer the ship in the right direction </p>
         </div>
       </div>
       <div class="skill-box bg-primary shadow-soft">
         <h2 class="skill-box__header">SEO</h2>
         <div class="skill-box__content">
-          <div class="img"></div>
+          <div class="img seo"></div>
           <p>I’ll do everything in my power to give your site a great ranking which will bring you more visitors.</p>
         </div>
       </div>
@@ -59,7 +59,8 @@
 .skill-container {
   display: relative;
   width: 110rem;
-  height: 55rem;
+  height: fit-content;
+  min-height: 55rem;
   margin: 10rem 0 0 0;
   color: var(--text-primary);
   border-radius: 14px;
@@ -80,6 +81,21 @@
   margin:0 0 0 3rem;
 }
 
+.vuejs {
+background: url('../assets/vuejs.svg') no-repeat center;
+background-size: 70%;
+}
+
+.css3 {
+background: url('../assets/CSS3.svg') no-repeat center;
+background-size: 55%;
+}
+
+.javascript {
+background: url('../assets/javascript.svg') no-repeat center;
+background-size: 70%;
+}
+
 .badge-container {
   margin-right: 3rem;
 }
@@ -89,6 +105,7 @@
   margin: 0 auto;
   font-size: 8rem;
   font-family: Righteous, sans-serif;
+  filter: brightness(110%) drop-shadow(1px 1px 1px var(--text-primary));
 }
 
 .skill-boxes-grid {
@@ -96,18 +113,22 @@
   width: 100%;
   margin: 0 auto;
   display: grid;
+  justify-content: center;
   justify-items: center;
   align-items: center;
-  grid-template:  repeat(2, 1fr) / 1fr 1fr;
+  // grid-template-rows: repeat(auto-fill;
+  grid-template-columns: repeat(auto-fit, 350px);
 }
 
 .skill-box {
   height: 16rem;
   width: 40rem;
   border-radius: 14px;
+  margin-top: 3.5rem;
 
 &__header {
   width: fit-content;
+  height: 30%;
   margin: 0 auto;
   font-size: 4rem;
   font-family: Righteous, sans-serif;
@@ -116,6 +137,7 @@
 
 
 &__content {
+  height: 70%;
   margin: 0rem 1rem 0 1rem;
 display: flex;
 justify-content: space-between;
@@ -125,7 +147,7 @@ align-items: center;
 }
 
 p {
-  margin-left: 1rem;
+  margin: 0 0 0 1rem;
   width: 90%;
   padding: 1rem;
   font-size: 14px;
@@ -139,6 +161,25 @@ p {
   width: 5rem;
   height: 5rem;
   background: #000;
+}
+
+.fast {
+  background: url('../assets/fast.png') no-repeat center;
+  background-size: 100%;
+}
+.cube {
+  background: url('../assets/cube.png') no-repeat center;
+  background-size: 100%;
+}
+
+.comms {
+  background: url('../assets/Coms.png') no-repeat center;
+  background-size: 100%;
+}
+
+.seo {
+  background: url('../assets/SEO.svg') no-repeat center;
+  background-size: 100%;
 }
 
 
